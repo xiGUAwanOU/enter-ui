2ad6fb28df3e29e7405022fe29fe324742eb28ba
Zihan Lin <lin@jacob.de>
2020-09-19 15:51:13 +0200

    Refactored the interface of the CheckboxGroup component again.

--- src/components/Checkbox/CheckboxGroup.vue
+++ src/components/Checkbox/CheckboxGroup.vue
@@ -16,12 +16,12 @@
       v-model="filterText"
     />
     <div class="e-checkbox-group__choice-wrapper">
-      <div v-for="(choice, key) in filteredModelValue" :key="key" class="e-checkbox-group__choice">
+      <div v-for="(choice, key) in filteredOptions" :key="key" class="e-checkbox-group__choice">
         <e-checkbox
           :disabled="disabled || choice.disabled"
           :size="size"
           :visual-type="visualType"
-          :model-value="choice.value"
+          :model-value="modelValue.includes(key)"
           @update:modelValue="toggleValue(key, $event)"
         >{{ choice.label }}</e-checkbox>
       </div>
@@ -34,10 +34,10 @@ import { PropType, computed, defineComponent, ref } from 'vue';
 
 import { ActionVisualType, ComponentSize } from '@/components/Shared/Common.types';
 
-interface ModelValue {
+interface Options {
   [key: string]: {
     label: string;
-    value: boolean;
+    value: string;
     disabled?: boolean;
   };
 }
@@ -46,7 +46,8 @@ export default defineComponent({
   name: 'ECheckboxGroup',
 
   props: {
-    modelValue: { type: Object as PropType<ModelValue>, required: true },
+    modelValue: { type: Array as PropType<string[]>, required: true },
+    options: { type: Object as PropType<Options>, required: true },
     disabled: { type: Boolean, default: false },
     filtered: { type: Boolean, default: false },
     filterPlaceholder: { type: String, default: '' },
@@ -58,9 +59,9 @@ export default defineComponent({
   setup(props, ctx) {
     const filterText = ref('');
 
-    const filteredModelValue = computed(() => {
-      const result: ModelValue = {};
-      for (const [ key, choice ] of Object.entries(props.modelValue)) {
+    const filteredOptions = computed(() => {
+      const result: Options = {};
+      for (const [ key, choice ] of Object.entries(props.options)) {
         if (choice.label.toLocaleLowerCase().includes(filterText.value.toLocaleLowerCase())) {
           result[key] = choice;
         }
@@ -69,15 +70,16 @@ export default defineComponent({
     });
 
     function toggleValue(key: string, value: boolean) {
-      const copiedModelValue = JSON.parse(JSON.stringify(props.modelValue));
-      copiedModelValue[key].value = value;
-
-      ctx.emit('update:modelValue', copiedModelValue);
+      if (value) {
+        ctx.emit('update:modelValue', [ ...props.modelValue, key ]);
+      } else {
+        ctx.emit('update:modelValue', props.modelValue.filter((selected) => selected !== key));
+      }
     }
 
     return {
       filterText,
-      filteredModelValue,
+      filteredOptions,
       toggleValue,
     };
   },
